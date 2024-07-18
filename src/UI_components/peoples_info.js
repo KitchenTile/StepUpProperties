da2b5571b0e200c83f847e5ce27cc94c2d7f6969
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-07-18 12:16:34 +0100

    Add files via upload

--- src/UI_components/peoples_info.js
+++ src/UI_components/peoples_info.js
@@ -1,13 +1,19 @@
-export default function peoplesInfo() {
-    const box = document.getElementById("box")
+import empoloyeeData from "../data/employeeData"
 
-    box.innerHTML = `<div class="image-box">
-         <img src="./src/images/Msolaimani.png" alt="img" />
+export default function peoplesInfo() {
+  const box = document.getElementById("box")
+  box.innerHTML = ""
+
+  empoloyeeData.forEach((employee, index) => {
+    box.innerHTML += `
+    <div id="employee${index}">
+      <div class="image-box" >
+          <img src=${employee.img} alt="img" />
       </div>
       <div class="text-box">
-        <h2> Morris Solaimani </h2>
-        <p>
-          Experienced Property Specialist with a demonstrated history of working in the real estate industry. Skilled in Negotiation, Lettings, Sales, Entrepreneurship, Asset Management, and Sales Management.
-        </p>
-      </div>`
+        <h2> ${employee.name} </h2>
+        <p>${employee.description}</p>
+      </div>
+    </div>`
+  })
 }
---
+++ src/data/employeeData.js
@@ -0,0 +1,9 @@
+
+const empoloyeeData = [
+    {name: "Morris Solaimani", img:"./src/images/Msolaimani.png" , description:"Experienced Property Specialist with a demonstrated history of working in the real estate industry. Skilled in Negotiation, Lettings, Sales, Entrepreneurship, Asset Management, and Sales Management."},
+    {name: "Morris Solaimani", img:"./src/images/Msolaimani.png" , description:"Experienced Property Specialist with a demonstrated history of working in the real estate industry. Skilled in Negotiation, Lettings, Sales, Entrepreneurship, Asset Management, and Sales Management."},
+    {name: "Morris Solaimani", img:"./src/images/Msolaimani.png" , description:"Experienced Property Specialist with a demonstrated history of working in the real estate industry. Skilled in Negotiation, Lettings, Sales, Entrepreneurship, Asset Management, and Sales Management."},
+    {name: "Morris Solaimani", img:"./src/images/Msolaimani.png" , description:"Experienced Property Specialist with a demonstrated history of working in the real estate industry. Skilled in Negotiation, Lettings, Sales, Entrepreneurship, Asset Management, and Sales Management."}
+];
+
+export default empoloyeeData;
