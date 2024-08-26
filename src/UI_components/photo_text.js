286e0c2f77f46de9cc26a0ee88084bc588d7cad9
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-08-26 21:21:30 +0100

    Add files via upload

--- src/UI_components/photo_text.js
+++ src/UI_components/photo_text.js
@@ -1,4 +1,6 @@
 import hendon_pic1 from "../images/hendonpicture.jpg"
+import hendon_pic2 from "../images/hendonpicture2.jpg"
+
 
 const photoTextContainer = () => {
     const photoTextContainer = document.querySelector(".photo-text-container");
@@ -10,10 +12,10 @@ const photoTextContainer = () => {
         <p>At StepUp Properties, we recognise that buying or selling a property is not just a transaction; it’s a significant milestone in your life. That’s why we approach every interaction with the utmost care, transparency, and integrity.</p>
     </div>
     <div class="text">
-        <p>At StepUp Properties, we recognise that buying or selling a property is not just a transaction; it’s a significant milestone in your life. That’s why we approach every interaction with the utmost care, transparency, and integrity.</p>
+        <p> Our commitment to staying ahead of market trends and utilising innovative technologies ensures that you have access to the latest information and tools to make informed decisions.</p>
     </div>
     <div class="photo">
-        <img src=${hendon_pic1} alt="hendon_pic1" />
+        <img src=${hendon_pic2} alt="hendon_pic1" />
     </div>
     
     `
